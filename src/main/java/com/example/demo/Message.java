2fdba9ec7b471176eab3bdc0e37e8455d32b7ad7
morganm22 <millermorgan98@gmail.com>
2018-12-07 18:20:34 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,74 @@
+package com.example.demo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+    private TemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new Java8TimeDialect());
+        engine.setTemplateResolver(templateResolver);
+        return engine;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 1)
+    @Size(max = 160)
+    private String content;
+
+    @DateTimeFormat(pattern = "")
+    private String posteddate;
+
+    @NotNull
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(String posteddate) {
+        this.posteddate = posteddate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+
+
+}
